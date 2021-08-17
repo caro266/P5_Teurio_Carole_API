f4ffcd270c7c23d6c8a22b7b21c0f4d415e8e176
caro266 <carole.teurio@gmail.com>
2021-08-17 09:49:56 +0200

    Evolution API

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
 
         df = get_UN_data()
         
-        page = st.sidebar.selectbox("Choisir votre page", ['Homepage', 'Exploration', 'Prediction'])
+        page = st.sidebar.selectbox("Select a page", ['Homepage', 'Exploration', 'Prediction'])
 
         if page == 'Homepage':
             st.text("Page d'acceuil.")
@@ -43,6 +43,9 @@ def main():
             vectorizer = joblib.load("tfidf_vectorizer.pkl", 'r')
             multilabel_binarizer = joblib.load("multilabel_binarizer.pkl", 'r')
 
+	    st.text("Exemple 1 :\n Titre: Git push existing repo to a new and different remote repo server? \n Body: <p>Say I have a repository and I want to clone this into my account at github to have my own playground aside from the more \"official\" repo on fedorahosted. What would be the steps to initially copy that over? Within github there is this nice \"fork\" button, but I can't use this for obvious reasons.</p> <p>And how would I track changes in the fedorahosted repo into the github one? </p>.")
+	    st.text("Exemple 1 :\n Titre: Debugging with command-line parameters in Visual Studio\n Body: <p>I'm developing a C++ command-line application in Visual Studio and need to debug it with command-line arguments. At the moment I just run the generated EXE file with the arguments I need (like this <code>program.exe -file.txt</code>) , but this way I can't debug. Is there somewhere I can specify the arguments for debugging?</p>")
+	    
             title = st.text_input('Input your title here: ') 
             body = st.text_input('Input your body here: ') 
 
@@ -71,8 +74,8 @@ def main():
                     tags_predict = multilabel_binarizer.inverse_transform(predict)
 
                     st.write(tags_predict)
-    except:
-        st.write("ERROR Exception")
+    except Exception as err:
+        st.write("ERROR Exception : ",err)
         
 if __name__ == '__main__':
     main()
